5d48930ad992e2c50a0aa2f1528430580e893503
s3pp1a <sebastian.bertuccio@gmail.com>
2026-01-30 22:32:31 +0100

    Create electron.js

---
+++ electron.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    icon: path.join(__dirname, 'dist', 'favicon.ico')
+  });
+
+  // Carica l'app da dist
+  mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
+
+  // Apri DevTools in sviluppo (commentare in produzione)
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
